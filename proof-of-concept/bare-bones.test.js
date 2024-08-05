8a6fd129afdb14edcefc9795286c2674d6831dc5
Connie <connieramirezphotography@gmail.com>
2024-08-05 12:36:18 +0100

    attempt calculator test

--- proof-of-concept/bare-bones.test.js
+++ proof-of-concept/bare-bones.test.js
@@ -1,10 +1,13 @@
 import { expect, test } from 'vitest';
 import { sum } from './sum.js'
-
+import {calculate} from './calculator.js'
 
   test('bare bones test should pass', () => {
     expect(sum(5, 7)).toBe(12)
   })
 
-  console.log(test);
+  test('bare bones test should pass', () => {
+    expect(calculate('+', 9, 10)).toBe(19)
+  })
+  
   
